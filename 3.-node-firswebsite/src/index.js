db5f657cbe09b8c2ba69353eebb9042a73cc3cf5
jonmolinna <jmolina2624@gmail.com>
2020-08-24 08:13:36 -0500

    Nodejs Firswebsite

---
+++ 3.-node-firswebsite/src/index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+// settings
+app.set('port', 3000);
+app.set('views', path.join(__dirname, 'views'));
+app.engine('html', require('ejs').renderFile);
+app.set('view engine','ejs');
+
+//middlewares
+// routes
+app.use(require('./routes/index.js'));
+
+//static files
+app.use(express.static(path.join(__dirname, 'public')));
+
+//listening the server
+app.listen(app.get('port'), () => {
+    console.log('Server on Port', app.get('port'));
+});
